0b1867da641be5eba05fa600e939ac89a9f03199
Khushal2706 <khushal2706patil@gmail.com>
2024-04-09 19:30:39 +0530

    added model to the project

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,9 +1,11 @@
 import * as path from "path"
+import ProductModel from "../models/product.model.js";
 
 export default class productController{
 
     getProducts(req,res){
-    console.log(path.resolve());
+        let products = ProductModel.get();
+        console.log(products);
        return res.sendFile(path.join(path.resolve(),'src','views','products.html'));
     }
 }
