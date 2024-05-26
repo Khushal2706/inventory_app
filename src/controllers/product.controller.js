132802006410efffe0f3e5159ebb49ffc5c3e107
Khushal2706 <khushal2706patil@gmail.com>
2024-05-26 18:09:17 +0530

    added data validator to validate the data given by the client

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -12,12 +12,31 @@ export default class productController{
 
 
     addForm(req,res){
-     return res.render("new-product");
+     return res.render("new-product", {errorMessage:null});
     }
 
 
     addNewProduct(req,res){
-      console.log(req.body);
+      // Validate data before uploading
+      // extract the data
+      const{name,price,imageUrl} = req.body;
+      let errors = [];
+      if(!name || name.trim() == ''){
+        errors.push("Name is required");
+      }
+
+      if(!price || parseFloat(price)<1){
+        errors.push("price must be a positive value ");
+      }
+
+      try{
+        const validUrl = new URL(imageUrl);
+      }catch(err){
+        errors.push("URL is invalid");
+      }
+      if(errors.length>0){
+        return res.render("new-product", {errorMessage: errors[0]});
+      }
       ProductModel.add(req.body)
       let products = ProductModel.get();
      return res.render('products',{products:products});
